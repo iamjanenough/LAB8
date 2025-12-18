8d458ec4dc4bd12333fe04b4251db458c1994f78
iamjanenough <jannilover20@gmail.com>
2025-12-18 16:38:30 +0700

    LAB8.2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+// ----------------------------
+// Function : findGrade
+// ----------------------------
 char findGrade(double x) {
     if(x > 90){
         return 'A';
@@ -14,4 +22,41 @@ char findGrade(double x) {
     else{
         return 'F';
     }
+}
+
+
+int main() {
+    int n;
+    cout << "Enter the number of students: ";
+    cin >> n;
+
+    string name[100];
+    double score[100];
+
+    cin.ignore(); 
+
+    for (int i = 0; i < n; i++) {
+        cout << "Name of student " << i + 1 << ": ";
+        getline(cin, name[i]);
+
+        cout << "Score of student " << i + 1 << ": ";
+        cin >> score[i];
+        cin.ignore(); 
+    }
+
+    cout << "---------------------------------------------" << endl;
+    cout << setw(25) << "Name"
+         << setw(8) << "Score"
+         << setw(8) << "Grade" << endl;
+    cout << "---------------------------------------------" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << setw(25) << name[i]
+             << setw(8) << score[i]
+             << setw(8) << findGrade(score[i]) << endl;
+    }
+
+    cout << "---------------------------------------------" << endl;
+
+    return 0;
 }
